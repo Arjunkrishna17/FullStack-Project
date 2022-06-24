17246bc34df481daa2b8737e8a88a8afe089bdb2
Arjunkrishna17 <107336846+Arjunkrishna17@users.noreply.github.com>
2022-06-24 18:14:17 +0530

    Digital clock 1st commit

---
+++ Project 2 digital cloc (JS)/index.js
@@ -0,0 +1,33 @@
+const hour_element = document.getElementById("hours");
+const min_element = document.getElementById("minutes");
+const sec_element = document.getElementById("seconds");
+const ampm_element = document.getElementById("ampm");
+
+function clock(){
+    let h =new Date().getHours();
+    let m =new Date().getMinutes();
+    let s =new Date().getSeconds();
+
+/*To update am and pm*/
+
+    if(h > 0){
+        h -=12;
+        ampm = "PM";
+    }
+
+    /*Now time is in single digit, so below code is to make it as two digit */
+
+    h = h<10 ? "0" + h :h;
+    m = m<10 ? "0" + m :m;
+    s = s<10 ? "0" + s :s;
+
+    hour_element.innerText = h;
+    min_element.innerText = m;
+    sec_element.innerText = s;
+    ampm_element.innerText = ampm;
+    setTimeout( () => {
+        clock();
+
+    },1000) //setTimeout(function,milliseconds)
+}
+clock();
